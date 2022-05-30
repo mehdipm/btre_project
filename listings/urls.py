e24434dfe383ae83567500a0e957151847f41de5
archangel <mehdip.m909@gmail.com>
2022-05-31 00:28:41 +0430

    modify admin page

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,10 @@
 from unicodedata import name
+
+from django.conf import settings
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = 'listings'
@@ -8,4 +12,4 @@ urlpatterns = [
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
     path('search/', views.search, name='search'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
