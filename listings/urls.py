39290cf67c1622a1d222267ac8061c016c0a598c
archangel <mehdip.m909@gmail.com>
2022-05-31 09:59:48 +0430

    pull data from database ang create pagination

--- listings/urls.py
+++ listings/urls.py
@@ -3,13 +3,10 @@ from unicodedata import name
 from django.conf import settings
 from . import views
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 app_name = 'listings'
 urlpatterns = [
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
     path('search/', views.search, name='search'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Listing
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 
 def index (request):
-    return render(request, 'listings/listings.html')
+    #pull data from database and order by date . publish or not
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    #pagination
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    #dictionary that will pass to template
+    context = {'listings': paged_listings}
+    return render(request, 'listings/listings.html', context)
 
-def listing (request, id):
+def listing (request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search (request):
